79fe4bf6c41f7f71e19ae9f4849c7d2d2da8aab8
Laura Fischer <lefischer@uc.cl>
2019-05-05 03:23:43 -0400

    major changes

---
+++ to_jpg.py
@@ -0,0 +1,52 @@
+import os
+import glob
+import cv2
+
+from PIL import Image
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Resize raw images to uniformed target size."
+    )
+    parser.add_argument(
+        "--raw-dir",
+        help="Directory path to raw images.",
+        default="./data/raw",
+        type=str,
+    )
+    parser.add_argument(
+        "--save-dir",
+        help="Directory path to save resized images.",
+        default="./data/images",
+        type=str,
+    )
+    parser.add_argument(
+        "--ext", help="Raw image files extension to resize.", default="jpg", type=str
+    )
+
+    args = parser.parse_args()
+
+    raw_dir = args.raw_dir
+    save_dir = args.save_dir
+    ext = args.ext
+    fnames = glob.glob(os.path.join(raw_dir, "*.{}".format(ext)))
+    os.makedirs(save_dir, exist_ok=True)
+    print(
+        "{} files to resize from directory `{}`".format(
+            len(fnames), raw_dir
+        )
+    )
+    for i, fname in enumerate(fnames):
+        print(".", end="", flush=True)
+        im = Image.open(fname)
+        rgb_im = im.convert('RGB')
+        rgb_im.save(save_dir +'/'+ str(i) + '_transforming.jpg')
+        img = cv2.imread(fname)
+    print(
+        "\nDone transforming {} files.\nSaved to directory: `{}`".format(
+            len(fnames), save_dir
+        )
+    )
